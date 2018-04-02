af7480230a1e66d77d245d9bad189df3de0943b3
Rogerisaza12 <isaza10121@gmail.com>
2018-04-02 10:42:02 -0500

    File headers

--- Assets/Scripts/TrainingData.cs
+++ Assets/Scripts/TrainingData.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿// <copyright file="TrainingData.cs" company="Up Up Down Studios">
+// Copyright (c) 2018 All Rights Reserved
+// </copyright>
+// <summary>Training data container.</summary>
+
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "TrainingData", menuName = "Assets/TrainingData", order = 2)]
 public class TrainingData : ScriptableObject {
--- Assets/Scripts/TrainingScreenData.cs
+++ Assets/Scripts/TrainingScreenData.cs
@@ -1,4 +1,9 @@
-﻿[System.Serializable]
+﻿// <copyright file="TrainingScreenData.cs" company="Up Up Down Studios">
+// Copyright (c) 2018 All Rights Reserved
+// </copyright>
+// <summary>Represent a training screen data.</summary>
+
+[System.Serializable]
 public class TrainingScreenData
 {
     public Exercise[] exercises;
